d82d6fe76280b36d5f227d919a2110a34fa11df6
Ciro Scognamiglio <c.scognamiglio@techgap.it>
2023-09-22 14:47:44 +0200

    chore(version): update bzr2 version

--- bzr2_installer.sh
+++ bzr2_installer.sh
@@ -27,7 +27,7 @@
 set -e
 
 main() {
-  bzr2_version_default="2.0.56.Alpha"
+  bzr2_version_default="2.0.58.Alpha"
   winearch_default="win64"
   force_reinstall_default="n"
   bzr2_zip_dir_default="."
