f29637c390a72848566ed5606db3fa6c245fa2d2
Ciro Scognamiglio <c.scognamiglio@techgap.it>
2023-09-29 14:08:28 +0200

    chore(mime-types): update x-fc & add x-fc-bsi

--- bzr2_installer.sh
+++ bzr2_installer.sh
@@ -35,11 +35,11 @@ main() {
   mime_types_association_default="y"
   mime_types=(
     application/ogg audio/flac audio/midi audio/mp2 audio/mpeg audio/prs.sid audio/x-ahx audio/x-cust audio/x-ddmf
-    audio/x-dw audio/x-dz audio/x-fc audio/x-flac+ogg audio/x-fp audio/x-hip audio/x-hip-7v audio/x-hip-coso
-    audio/x-hip-st audio/x-hip-st-coso audio/x-it audio/x-lds audio/x-m2 audio/x-mcmd audio/x-mdx audio/x-mmdc
-    audio/x-mo3 audio/x-mod audio/x-mpegurl audio/x-mptm audio/x-okt audio/x-prun audio/x-psm audio/x-pt3 audio/x-s3m
-    audio/x-sc2 audio/x-sc68 audio/x-scl audio/x-sid2 audio/x-sndh audio/x-soundmon audio/x-spc audio/x-spl audio/x-stk
-    audio/x-stm audio/x-sun audio/x-sunvox audio/x-symmod audio/x-tfmx audio/x-umx audio/x-v2m audio/x-vgm
+    audio/x-dw audio/x-dz audio/x-fc audio/x-fc-bsi audio/x-flac+ogg audio/x-fp audio/x-hip audio/x-hip-7v
+    audio/x-hip-coso audio/x-hip-st audio/x-hip-st-coso audio/x-it audio/x-lds audio/x-m2 audio/x-mcmd audio/x-mdx
+    audio/x-mmdc audio/x-mo3 audio/x-mod audio/x-mpegurl audio/x-mptm audio/x-okt audio/x-prun audio/x-psm audio/x-pt3
+    audio/x-s3m audio/x-sc2 audio/x-sc68 audio/x-scl audio/x-sid2 audio/x-sndh audio/x-soundmon audio/x-spc audio/x-spl
+    audio/x-stk audio/x-stm audio/x-sun audio/x-sunvox audio/x-symmod audio/x-tfmx audio/x-umx audio/x-v2m audio/x-vgm
     audio/x-vorbis+ogg audio/x-wav audio/x-xm
   )
 
@@ -514,19 +514,37 @@ EOF
   <mime-type type="audio/x-fc">
     <comment>Future Composer</comment>
     <icon name="audio-x-generic"/>
+    <magic>
+      <match type="string" value="SMOD" offset="0"/>
+      <match type="string" value="FC14" offset="0"/>
+    </magic>
     <glob-deleteall/>
-    <glob pattern="bsi.*"/>
     <glob pattern="fc.*"/>
     <glob pattern="fc13.*"/>
     <glob pattern="fc14.*"/>
     <glob pattern="smc.*"/>
     <glob pattern="smod.*"/>
-    <glob pattern="*.bsi"/>
     <glob pattern="*.fc"/>
     <glob pattern="*.fc13"/>
     <glob pattern="*.fc14"/>
     <glob pattern="*.smc"/>
     <glob pattern="*.smod"/>
+  </mime-type>
+</mime-info>
+EOF
+
+  cat <<'EOF' >"$mime_packages_dir_user/audio-x-fc-bsi.xml"
+<?xml version="1.0" encoding="utf-8"?>
+<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+  <mime-type type="audio/x-fc-bsi">
+    <comment>Future Composer (BSI)</comment>
+    <icon name="audio-x-generic"/>
+    <magic>
+      <match type="string" value="FUCO" offset="0"/>
+    </magic>
+    <glob-deleteall/>
+    <glob pattern="bsi.*"/>
+    <glob pattern="*.bsi"/>
   </mime-type>
 </mime-info>
 EOF
