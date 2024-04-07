704de2e04f324f697d18f29f1866feda374a662a
Ciro Scognamiglio <c.scognamiglio@techgap.it>
2024-04-07 16:55:25 +0200

    feat(aur): add bzr-player.sh

---
+++ aur/bzr-player.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# NAME
+#     bzr-player.sh - BZR Player 2 linux runner
+#
+# SYNOPSIS
+#     ./bzr-player.sh [target(s)]
+#
+# EXAMPLES
+#     ./bzr-player.sh
+#         run bzr2
+#
+#     ./bzr-player.sh file1 file2 dir1 dir2
+#         run bzr2 with selected files and/or directories as arguments
+#
+# AUTHOR
+#     Ciro Scognamiglio
+
+set -e
+bzr2="bzr-player"
+export WINEPREFIX="$HOME/.$bzr2"
+
+if [ ! -d "$HOME/.$bzr2" ]; then
+  mkdir -p "$HOME/.$bzr2"
+  #TODO
+fi
+
+WINEDEBUG=-all WINEARCH="win32" wine "/usr/share/$bzr2/BZRPlayer.exe" "$@" &
