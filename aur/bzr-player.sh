5d0319d45efc975d9826d93c12ee2f3918bb6b2b
Ciro Scognamiglio <c.scognamiglio@techgap.it>
2024-04-10 22:08:30 +0200

    chore(aur): update launcher

--- aur/bzr-player.sh
+++ aur/bzr-player.sh
@@ -19,11 +19,11 @@
 set -e
 
 bzr2="bzr-player"
-bzr2_path="/usr/share/$bzr2"
-bzr2_exe="BZRPlayer.exe"
+bzr2_path_sys="/usr/share/$bzr2"
+bzr2_path_home="$HOME/.$bzr2"
 
-export WINEDEBUG=-all
-export WINEPREFIX="$HOME/.$bzr2/wine"
+export WINEDEBUG=-all #TODO
+export WINEPREFIX="$bzr2_path_home/wine"
 
 if [ "$(uname -m)" == "x86_64" ]; then
   export WINEARCH="win64"
@@ -31,21 +31,51 @@ else
   export WINEARCH="win32"
 fi
 
-if [ ! -d "$HOME/.$bzr2" ]; then
+if [ ! -d "$bzr2_path_home" ]; then
   mkdir -p "$WINEPREFIX"
 
-  #winetricks nocrashdialog
+  # winetricks nocrashdialog
   wine reg add "HKEY_CURRENT_USER\Software\Wine\WineDbg" /v ShowCrashDialog /t REG_DWORD /d 0 /f
 
-  #winetricks autostart_winedbg=disabled (never worked in winetricks)
+  # winetricks autostart_winedbg=disabled (never worked in winetricks)
   wine reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\AeDebug" /v Debugger /t REG_SZ /d "-" /f
 
-  #ln -s "$bzr2_path/$bzr2_exe" "$HOME/.$bzr2/$bzr2"
-  #mkdir -p "$HOME/.$bzr2/plugin"
-  #ln -s -T "$bzr2_path/plugin/config" "$HOME/.$bzr2/plugin/config"
-  #ln -s "$bzr2_path/settings.ini" "$HOME/.$bzr2"
-  #TODO link songlengths
+  # --- app data setup ---
+  ln -s "$bzr2_path_sys/BZRPlayer.exe" "$bzr2_path_home/$bzr2"
+  ln -s "$bzr2_path_sys/BZRPlayerTest.exe" "$bzr2_path_home/$bzr2-test"
+
+  readarray -d '' root_dlls < <(find "$bzr2_path_sys" -maxdepth 1 -type f -iname '*.dll' -print0)
+
+  for root_dll in "${root_dlls[@]}"; do
+    ln -s "$root_dll" "$bzr2_path_home"
+  done
+
+  ln -s "$bzr2_path_sys/gm.dls" "$bzr2_path_home"
+  ln -s "$bzr2_path_sys/imageformats" "$bzr2_path_home"
+  ln -s "$bzr2_path_sys/layouts" "$bzr2_path_home"
+  ln -s "$bzr2_path_sys/platforms" "$bzr2_path_home"
+  mkdir -p "$bzr2_path_home/plugin"
+  ln -s "$bzr2_path_sys/plugin/orgsamples" "$bzr2_path_home/plugin"
+  ln -s "$bzr2_path_sys/plugin/SC68" "$bzr2_path_home/plugin"
+  ln -s "$bzr2_path_sys/plugin/sid" "$bzr2_path_home/plugin"
+  ln -s "$bzr2_path_sys/plugin/uade" "$bzr2_path_home/plugin"
+
+  readarray -d '' plugin_dlls < <(find "$bzr2_path_sys/plugin" -maxdepth 1 -type f -iname '*.dll' -print0)
+
+  for plugin_dll in "${plugin_dlls[@]}"; do
+    ln -s "$plugin_dll" "$bzr2_path_home/plugin"
+  done
+
+  ln -s "$bzr2_path_sys/resources" "$bzr2_path_home"
+  # END --- app data setup ---
+
+  # --- user data setup ---
+  mkdir -p "$bzr2_path_home/playlists"
+  mkdir -p "$bzr2_path_home/plugin/config"
+  cp -a "$bzr2_path_sys/Songlengths.md5" "$bzr2_path_home"
+  cp -a "$bzr2_path_sys/Songlengths.txt" "$bzr2_path_home"
+  # END --- user data setup ---
 fi
 
-#wine "$HOME/.$bzr2/$bzr2" "$@" &
-wine "$bzr2_path/$bzr2_exe" "$@" &
+export WINEDEBUG=warn #TODO
+wine "$bzr2_path_home/$bzr2" "$@" &
